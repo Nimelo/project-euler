123e9302a1a6d30e00c0a486a38bd48ae536c385
Mateusz Gąsior <mr.nimelo@gmail.com>
2017-10-20 21:50:46 +0100

    Improvements in Problem 2.

--- problems/problem-2/FibonacciSeries.py
+++ problems/problem-2/FibonacciSeries.py
@@ -2,19 +2,31 @@ from itertools import takewhile
 
 
 def fibonacci_series():
-    last = 1
-    current = 1
+    last, current = 0, 1
 
-    yield last
     while True:
-        tmp = current
-        current += last
-        last = tmp
+        current, last = current + last, current
         yield current
 
 
+def even_fibonacci_series():
+    last, current = 0, 1
+
+    while True:
+        current, last = current + last, current
+        current, last = current + last, current
+        current, last = current + last, current
+        yield last
+
+
 if __name__ == "__main__":
-    smaller_than_4_millions = takewhile(lambda x: x < 4e6 + 1, fibonacci_series())
+    n = 4e6
+    smaller_than_4_millions = takewhile(lambda x: x < n + 1, fibonacci_series())
     even_numbers = filter(lambda x: x % 2 == 0, smaller_than_4_millions)
-    sum = sum(list(even_numbers))
-    print(f"The result is {sum}")
+    result = sum(list(even_numbers))
+    print(f"The result is {result}")
+
+    smaller_than_4_millions = takewhile(lambda x: x < n + 1, even_fibonacci_series())
+    result = sum(list(smaller_than_4_millions))
+
+    print(f"The result is {result}")
